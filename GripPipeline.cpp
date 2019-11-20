ce13d9809fc0eb25bb275e373f12fd953356567f
Nathan Johnson <najohnson446@gmail.com>
2019-11-19 19:26:04 -0600

    removed line code

--- GripPipeline.cpp
+++ GripPipeline.cpp
@@ -39,16 +39,6 @@ void GripPipeline::Process(cv::Mat& source0){
 	//input
 	std::vector<std::vector<cv::Point> > convexHullsContours = filterContoursOutput;
 	convexHulls(convexHullsContours, this->convexHullsOutput);
-	//Step Find_Lines0:
-	//input
-	cv::Mat findLinesInput = source0;
-	findLines(findLinesInput, this->findLinesOutput);
-	//Step Filter_Lines0:
-	//input
-	std::vector<Line> filterLinesLines = findLinesOutput;
-	double filterLinesMinLength = 40.0;  // default Double
-	double filterLinesAngle[] = {0, 360.0};
-	filterLines(filterLinesLines, filterLinesMinLength, filterLinesAngle, this->filterLinesOutput);
 }
 
 /**
@@ -78,20 +68,6 @@ std::vector<std::vector<cv::Point> >* GripPipeline::GetFilterContoursOutput(){
  */
 std::vector<std::vector<cv::Point> >* GripPipeline::GetConvexHullsOutput(){
 	return &(this->convexHullsOutput);
-}
-/**
- * This method is a generated getter for the output of a Find_Lines.
- * @return LinesReport output from Find_Lines.
- */
-std::vector<Line>* GripPipeline::GetFindLinesOutput(){
-	return &(this->findLinesOutput);
-}
-/**
- * This method is a generated getter for the output of a Filter_Lines.
- * @return LinesReport output from Filter_Lines.
- */
-std::vector<Line>* GripPipeline::GetFilterLinesOutput(){
-	return &(this->filterLinesOutput);
 }
 	/**
 	 * Segment an image based on hue, saturation, and luminance ranges.
@@ -173,53 +149,6 @@ std::vector<Line>* GripPipeline::GetFilterLinesOutput(){
 			cv::convexHull(cv::Mat((inputContours)[i]), hull[i], false);
 		}
 		outputContours = hull;
-	}
-
-	/**
-	 * Finds all line segments in an image.
-	 *
-	 * @param input The image on which to perform the find lines.
-	 * @param lineList The output where the lines are stored.
-	 */
-	void GripPipeline::findLines(cv::Mat &input, std::vector<Line> &lineList) {
-		cv::Ptr<cv::LineSegmentDetector> lsd = cv::createLineSegmentDetector(cv::LSD_REFINE_STD);
-		std::vector<cv::Vec4i> lines;
-		lineList.clear();
-		if (input.channels() == 1) {
-			lsd->detect(input, lines);
-		} else {
-			// The line detector works on a single channel.
-			cv::Mat tmp;
-			cv::cvtColor(input, tmp, cv::COLOR_BGR2GRAY);
-			lsd->detect(tmp, lines);
-		}
-		// Store the lines in the LinesReport object
-		if (!lines.empty()) {
-			for (int i = 0; i < lines.size(); i++) {
-				cv::Vec4i line = lines[i];
-				lineList.push_back(Line(line[0], line[1], line[2], line[3]));
-			}
-		}
-	}
-
-	/**
-	 * Filters out lines that do not meet certain criteria.
-	 *
-	 * @param inputs The lines that will be filtered.
-	 * @param minLength The minimum length of a line to be kept.
-	 * @param angle The minimum and maximum angle of a line to be kept.
-	 * @param outputs The output lines after the filter.
-	 */
-	void GripPipeline::filterLines(std::vector<Line> &inputs, double minLength, double angle[], std::vector<Line> &outputs) {
-	outputs.clear();
-	for (Line line: inputs) {
-		if (line.length()>abs(minLength)) {
-			if ((line.angle() >= angle[0] && line.angle() <= angle[1]) ||
-					(line.angle() + 180.0 >= angle[0] && line.angle() + 180.0 <=angle[1])) {
-				outputs.push_back(line);
-			}
-		}
-	}
 	}
 
 
